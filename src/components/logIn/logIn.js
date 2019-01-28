4171f3efc6e8ebb9504d0fc708698a0769bd6bf0
cris93g <37388115+cris93g@users.noreply.github.com>
2019-01-27 23:12:43 -0600

    redux was added putting auth to front end

--- src/components/logIn/logIn.js
+++ src/components/logIn/logIn.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { getItems } from "../../redux/ducks/userReducer";
 
 const Login = props => {
   console.log(props);
