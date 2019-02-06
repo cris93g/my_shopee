58ce5013c0b54cc391a98467167fb322928825e7
cris93g <37388115+cris93g@users.noreply.github.com>
2019-02-06 15:11:06 -0600

    auth works front end

--- src/components/logIn/logIn.js
+++ src/components/logIn/logIn.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import { connect } from "react-redux";
-const Login = props => {
-  console.log(props);
+import Auth from "../Auth/Auth";
+const LogIn = props => {
+	console.log(props);
 
-  return (
-    <div>
-      <a href={process.env.REACT_APP_LOGIN}>
-        <button>Log in</button>
-      </a>
-    </div>
-  );
+	return (
+		<div>
+			<Auth />
+			<a href={process.env.REACT_APP_LOGIN}>
+				<button>LOG IN</button>
+			</a>
+		</div>
+	);
 };
 const mapStatetoProps = state => state;
-export default connect(mapStatetoProps)(Login);
+export default connect(mapStatetoProps)(LogIn);
--- src/redux/ducks/userReducer.js
+++ src/redux/ducks/userReducer.js
@@ -3,45 +3,44 @@ import axios from "axios";
 const GET_USER = "GET_USER";
 const LOG_OUT = "LOG_OUT";
 export function getUser() {
-  return {
-    type: GET_USER,
-    payload: axios.get("/me")
-  };
+	return {
+		type: GET_USER,
+		payload: axios.get("/me")
+	};
 }
 export function logout() {
-  return {
-    type: LOG_OUT,
-    payload: axios.get("/logout")
-  };
+	return {
+		type: LOG_OUT,
+		payload: axios.get("/logout")
+	};
 }
 const initialState = {
-  user: {},
-  isAuthed: false,
-  userInfo: {}
+	user: {},
+	isAuthed: false
 };
 
 export default function itemsReducer(state = initialState, action) {
-  switch (action.type) {
-    case `${GET_USER}_FULFILLED`:
-      return {
-        ...state,
-        user: action.payload.data,
-        isAuthed: true
-      };
-    case `${GET_USER}_REJECTED`:
-      return {
-        ...state,
-        isAuthed: false
-      };
+	switch (action.type) {
+		case `${GET_USER}_FULFILLED`:
+			return {
+				...state,
+				user: action.payload.data,
+				isAuthed: true
+			};
+		case `${GET_USER}_REJECTED`:
+			return {
+				...state,
+				isAuthed: false
+			};
 
-    case `${LOG_OUT}_FULFILLED`:
-      return {
-        ...state,
-        user: action.payload.data,
-        isAuthed: true
-      };
+		case `${LOG_OUT}_FULFILLED`:
+			return {
+				...state,
+				user: action.payload.data,
+				isAuthed: true
+			};
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 }
